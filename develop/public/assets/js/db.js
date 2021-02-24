d257e67b3cbf122411e826a90b8033259b049aaa
Stephen Keenan <stephenkeenan02@gmail.com>
2021-02-23 21:48:53 -0500

    db.js comments

---
+++ develop/public/assets/js/db.js
@@ -0,0 +1,28 @@
+// needs to connect to indexdb
+// needs to set up store for pending
+// on success, check nagivator to see if online; if yes, then run function checkdatabase
+// checkdatabase function
+
+fetch("/api/transaction", {
+    method: "POST",
+    body: JSON.stringify(transaction),
+    headers: {
+      Accept: "application/json, text/plain, */*",
+      "Content-Type": "application/json"
+    }
+  })
+  .then(response => {    
+    return response.json();
+  })
+  .then(data => {
+    if (data.errors) {
+      errorEl.textContent = "Missing Information";
+    }
+    else {
+      // clear form
+      nameEl.value = "";
+      amountEl.value = "";
+    }
+  })
+
+// saverecord function
