eb7bca69f2580f74c89787f14e953b114e16e229
Stephen Keenan <stephenkeenan02@gmail.com>
2021-02-23 22:13:49 -0500

    complete db.js

--- develop/public/assets/js/db.js
+++ develop/public/assets/js/db.js
@@ -1,28 +1,58 @@
 // needs to connect to indexdb
 // needs to set up store for pending
-// on success, check nagivator to see if online; if yes, then run function checkdatabase
+// on success, check nagivator to see if online; if yes, then run function checkdatabase; function save recrod
 // checkdatabase function
 
-fetch("/api/transaction", {
-    method: "POST",
-    body: JSON.stringify(transaction),
-    headers: {
-      Accept: "application/json, text/plain, */*",
-      "Content-Type": "application/json"
-    }
-  })
-  .then(response => {    
-    return response.json();
-  })
-  .then(data => {
-    if (data.errors) {
-      errorEl.textContent = "Missing Information";
-    }
-    else {
-      // clear form
-      nameEl.value = "";
-      amountEl.value = "";
-    }
-  })
+let db; 
+// We request a database instance.
+ const request = indexedDB.open("budgetDatabase", 1);
+  
+ request.onupgradeneeded = event => {
+     let db = event.target.result
+     db.createObjectStore("pending", {autoIncrement: true})
+ };
 
-// saverecord function
+ // This returns a result that we can then manipulate.
+ request.onsuccess = event => {
+   // console.log(request.result);
+   db = event.target.result;
+   if (navigator.onLine){
+       checkDatabase()
+   }
+ };
+
+ function saveRecord(record){
+     const transaction = db.transaction(["pending"], "readwrite");
+     const store = transaction.objectStore("pending");
+     store.add(record)
+ };
+
+ function checkDatabase(){
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
+    getAll.onsuccess = () => {
+        if (getAll.result.length > 0){
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                  Accept: "application/json, text/plain, */*",
+                  "Content-Type": "application/json"
+                }
+              })
+              .then(response => {    
+                return response.json();
+              })
+              .then(() => {
+                const transaction = db.transaction(["pending"], "readwrite");
+                const store = transaction.objectStore("pending");
+                store.clear()
+              })
+        }
+    }
+ };
+
+ window.addEventListener("online", checkDatabase)
+
+ //review week 17 class activities
